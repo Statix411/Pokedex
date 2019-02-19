f858b5303324df252f6c594c355f6a942ff04f82
Eugene <k2j3.statix@gmail.com>
2019-02-19 21:08:00 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
-const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 let config = {
   entry: './src/index.js',
   output: {
-      path: path.resolve(__dirname, './dist'),
-      filename: 'main.js',
-      publicPath: 'dist/'
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js',
+    publicPath: 'dist/'
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: 'babel-loader'
       },
       {
 
         test: /\.(js|jsx)$/,
-        
+
         exclude: /node_modules/,
         use: ['babel-loader']
       },
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
-          //fallback: "style-loader",
-          use: "css-loader"
+          // fallback: "style-loader",
+          use: 'css-loader'
         })
       }
     ]
   },
   plugins: [
-    new ExtractTextPlugin("styles.css")
+    new ExtractTextPlugin('styles.css')
   ]
-};
+}
 
-module.exports = config;
+module.exports = config
